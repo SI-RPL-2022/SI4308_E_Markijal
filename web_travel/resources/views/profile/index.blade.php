4b381b3433a44eaf37e5764f243b4a1cc7a14a1b
dendysrprdn <dendysrprdn@student.telkomuniversity.ac.id>
2022-04-27 16:07:23 +0700

    Done: Detail Profile resolve ref #4

---
+++ web_travel/resources/views/profile/index.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.frontend')
+
+@section('content')
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            Profil Saya
+        </div>
+        <div class="card-body">
+            @if ($message = Session::get('success'))
+            <div class="alert alert-success">
+                {{$message}}
+            </div>
+            @endif
+            <div class="form-group">
+                <label for="">Nama</label>
+                <input type="text" name="name" value="{{ Auth::user()->name }}" id="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email" name="email" value="{{ Auth::user()->email }}" id="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Alamat</label>
+                <input type="text" name="alamat" value="{{ Auth::user()->alamat }}" id="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">No HP</label>
+                <input type="number" name="no_hp" value="{{ Auth::user()->no_hp }}" id="" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Password <small class="text-danger">*Isi bila ingin mengganti</small></label>
+                <input type="password" name="password" id="" class="form-control">
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-header">
+            Riwayat Transaksi
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Nama</th>
+                        <th>Jumlah Orang</th>
+                        <th>Paket Dipilih</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($transactions as $item)
+                    <tr>
+                        <th>{{ $loop->iteration }}</th>
+                        <td>
+                            {{$item->user->name}}
+                        </td>
+                        <td>
+                            {{$item->jumlah_orang}}
+                        </td>
+                        <td>
+                            <a href="{{ route('paket.show', $item->package->id) }}">{{$item->package->nama_paket}}</a>
+                        </td>
+                        <td>
+                            {{$item->total}}
+                        </td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <th>Belum ada transaksi...</th>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
