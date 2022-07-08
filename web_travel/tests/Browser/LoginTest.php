55890a186a518e64c37247107dc50108218691d8
Muhammad Haikal Qolby <haikalqolbi@student.telkomuniversity.ac.id>
2022-07-08 14:50:30 +0700

    Markijal --  Automated Testing

--- web_travel/tests/Browser/LoginTest.php
+++ web_travel/tests/Browser/LoginTest.php
@@ -15,13 +15,13 @@ class LoginTest extends DuskTestCase
      * @return void
      */
     
-    public function login_user()
+    public function login_admin()
     {
 
         $this->browse(function (browser $browser){
             $browser->visit('/')
                     ->clickLink('Login')
-                    ->type('email', 'taylor@laravel.com')
+                    ->type('email', 'admin@mail.com')
                     ->type('password', 'password')
                     ->press('Login')
                     ->assertPathIs('/home');
@@ -35,18 +35,115 @@ class LoginTest extends DuskTestCase
      * @return void
      */
         
-    public function logout_user()
+    public function logout_admin()
     {
 
         $this->browse(function (browser $browser) {
             $browser->visit('/')
                     ->clickLink('Login')
-                    ->type('email', 'taylor@laravel.com')
+                    ->type('email', 'admin@mail.com')
                     ->type('password', 'password')
                     ->press('Login')
                     ->assertPathIs('/home')
                     ->visit('/logout')
                     ->assertPathIs('/logout');
+        });
+    }
+    
+    /**
+     * A Dusk test example.
+     * @test
+     * @group category
+     * @return void
+     */
+    
+    public function create_category()
+    {
+
+        $this->browse(function (browser $browser){
+            $browser->visit('/')
+                    ->clickLink('Login')
+                    ->type('email', 'admin@mail.com')
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/home')
+                    ->assertSee('Dashboard')
+                    ->visit('/category')
+                    ->visit('/category/create')
+                    ->type('nama', 'Vacation')
+                    ->press('Submit')
+                    ->assertPathIs('/category')
+                    ->assertSee('Vacation');
+                  
+        });
+    }
+    /**
+     * A Dusk test example.
+     * @test
+     * @group package
+     * @return void
+     */
+    
+    public function create_package()
+    {
+ 
+        $this->browse(function (browser $browser) {
+            $browser->visit('/')
+                    ->clickLink('Login')
+                    ->type('email', 'admin@mail.com')
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/home')
+                    ->assertSee('Dashboard')
+                    ->visit('/category')
+                    ->visit('/category/create')
+                    ->type('nama', 'Haji')
+                    ->press('Submit')
+                    ->assertPathIs('/category')
+                    ->assertSee('Haji')
+                    ->visit('/package')
+                    ->visit('/package/create')
+                    ->type('nama_paket', 'Haji')
+                    ->type('harga_satuan', '500')
+                    ->type('tujuan', 'arab')
+                    ->type('stok', '20')
+                    ->select('category_id','3')
+                    ->press('Submit')
+                    ->assertPathIs('/package')
+                    ->assertSee('Haji');
+                  
+        });
+    }
+    /**
+     * A Dusk test example.
+     * @test
+     * @group user
+     * @return void
+     */
+    
+    public function create_user()
+    {
+       
+        $this->browse(function (browser $browser){
+            $browser->visit('/')
+                    ->clickLink('Login')
+                    ->type('email', 'admin@mail.com')
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/home')
+                    ->assertSee('Dashboard')
+                    ->visit('/user')
+                    ->visit('/user/create')
+                    ->type('name', 'dendy')
+                    ->type('email','dendy@gmail.com')
+                    ->type('password', 'password')
+                    ->type('no_hp','081123456789')
+                    ->type('alamat','jalan bisa ya')
+                    ->select('role','user')
+                    ->press('Submit')
+                    ->assertPathIs('/user')
+                    ->assertSee('dendy');
+                  
         });
     }
     /**
@@ -75,96 +172,63 @@ class LoginTest extends DuskTestCase
     /**
      * A Dusk test example.
      * @test
-     * @group category
+     * @group users
      * @return void
      */
     
-    public function create_category()
+    public function booking_user()
     {
+        $today = now();
+        $this->browse(function (browser $browser)use($today){
+            $browser->visit('/')
+                    ->clickLink('Login')
+                    ->type('email', 'test@mail.com')
+                    ->type('password', 'password')
+                    ->press('Login')
+                    ->assertPathIs('/')
+                    ->assertSee('MARKIJAL')
+                    ->visit('/paket')
+                    ->press('Book Now')
+                    ->type('tanggal_pesan',$today->day)
+                    ->type('tanggal_pesan', $today->month)
+                    ->type('tanggal_pesan', $today->year)
+                    ->press('Checkout')
+                    ->assertPathIs('/profile')
+                    ->assertsee('Profil Saya');
 
-        $this->browse(function (browser $browser){
-            $browser->visit('/')
-                    ->clickLink('Login')
-                    ->type('email', 'taylor@laravel.com')
-                    ->type('password', 'password')
-                    ->press('Login')
-                    ->assertPathIs('/home')
-                    ->assertSee('Dashboard')
-                    ->visit('/category')
-                    ->visit('/category/create')
-                    ->type('nama', 'Haji')
-                    ->press('Submit')
-                    ->assertPathIs('/category')
-                    ->assertSee('Haji');
                   
         });
     }
     /**
      * A Dusk test example.
      * @test
-     * @group package
+     * @group up
      * @return void
      */
     
-    public function create_package()
+    public function update_users()
     {
- 
-        $this->browse(function (browser $browser) {
+        $today = now();
+        $this->browse(function (browser $browser)use($today){
             $browser->visit('/')
                     ->clickLink('Login')
-                    ->type('email', 'taylor@laravel.com')
+                    ->type('email', 'test@mail.com')
                     ->type('password', 'password')
                     ->press('Login')
-                    ->assertPathIs('/home')
-                    ->assertSee('Dashboard')
-                    ->visit('/category')
-                    ->visit('/category/create')
-                    ->type('nama', 'Haji')
-                    ->press('Submit')
-                    ->assertPathIs('/category')
-                    ->assertSee('Haji')
-                    ->visit('/package')
-                    ->visit('/package/create')
-                    ->type('nama_paket', 'Haji')
-                    ->type('harga_satuan', '500')
-                    ->type('tujuan', 'arab')
-                    ->type('stok', '20')
-                    ->select('category_id','1')
-                    ->press('Submit')
-                    ->assertPathIs('/package')
-                    ->assertSee('Haji');
-                  
-        });
-    }
-    /**
-     * A Dusk test example.
-     * @test
-     * @group user
-     * @return void
-     */
-    
-    public function create_user()
-    {
-       
-        $this->browse(function (browser $browser){
-            $browser->visit('/')
-                    ->clickLink('Login')
-                    ->type('email', 'taylor@laravel.com')
-                    ->type('password', 'password')
-                    ->press('Login')
-                    ->assertPathIs('/home')
-                    ->assertSee('Dashboard')
-                    ->visit('/user')
-                    ->visit('/user/create')
-                    ->type('name', 'Haji')
-                    ->type('email','haji@gmail.com')
-                    ->type('password', 'password')
-                    ->type('no_hp','081123456789')
+                    ->assertPathIs('/')
+                    ->assertSee('MARKIJAL')
+                    ->visit('/profile')
+                    ->assertsee('Profil Saya')
+                    ->type('name', 'halo')
+                    ->type('email','halo@gmail.com')
                     ->type('alamat','jalan bisa ya')
-                    ->select('role','user')
-                    ->press('Submit')
-                    ->assertPathIs('/user')
-                    ->assertSee('Haji');
+                    ->type('no_hp','081123456789')
+                    ->type('password', 'password')
+                    ->press('Update')
+                    ->assertSee('Profil Saya');
+                    
+                    
+
                   
         });
     }
